b3f0137185127fcef1eb2bd02d1c0a943aa627f5
Ray Zhang <1789958884@qq.com>
2023-11-09 10:43:29 +0800

    feat: 精简代码

--- main.cpp
+++ main.cpp
@@ -17,22 +17,32 @@ void printRed(const std::string& message)
     printf("\033[31m%s\033[0m\n", message.c_str());
 }
 
-// 统计给定路径下的文件数量
-std::size_t countFilesInDirectory(const fs::path& path)
+// 用于在控制台打印绿色文本的函数
+void printGreen(const std::string& message)
 {
-    std::size_t fileCount = 0;
+    printf("\033[32m%s\033[0m\n", message.c_str());
+}
 
-    // 使用递归目录迭代器遍历所有文件和子目录
-    for (const auto& entry : fs::recursive_directory_iterator(path)) {
-        if (fs::is_regular_file(entry)) {
-            ++fileCount; // 如果是文件，则增加计数
-        }
-    }
-
-    return fileCount;
+// 用于在控制台打印黄色文本的函数
+void printYellow(const std::string& message)
+{
+    printf("\033[33m%s\033[0m\n", message.c_str());
 }
 
 using RemoveResult = std::pair<bool, std::string>;
+
+void printResult(const RemoveResult& res)
+{
+    const auto& [isOk, message] = res;
+    if (isOk) {
+        printGreen(message);
+    }
+    else {
+        printRed(message);
+    }
+
+}
+
 // 删除给定路径的文件或文件夹
 RemoveResult removeData(const fs::path& dataToRemove)
 {
@@ -48,7 +58,7 @@ RemoveResult removeData(const fs::path& dataToRemove)
                 }
             }
             else if (fs::is_directory(dataToRemove)) {
-                // 递归删除目录
+                // todo: 优化性能
                 const auto removed_count = fs::remove_all(dataToRemove);
                 message = std::format("Removed {} item(s) from directory: {}", removed_count, dataToRemove.string());
             }
@@ -68,59 +78,39 @@ RemoveResult removeData(const fs::path& dataToRemove)
 void syncWait(std::vector<std::future<RemoveResult>>& threads)
 {
     for (auto& th : threads) {
-        const auto& [isOk, message] = th.get();
-        if (isOk) {
-            std::cout << message << std::endl;
-        }
-        else {
-            printRed(message);
-        }
+        printResult(th.get());
     }
     threads.clear();
 }
 
 int main(int argc, char* argv[])
 {
-    //ray::utils::addToContextMenu("rayrm", std::format(R"({}/rayrm.exe %1)", fs::current_path().string()));
-    if (argc != 2) {
-        printRed("Usage: rayrm d:/test d:/test2 d:/test3.txt ...");
+    if (argc < 2) {
+        printYellow("Usage:\n rayrm d:/test d:/test2 d:/test3.txt ...");
         return 1;
     }
-    const auto maxThreadCount = std::thread::hardware_concurrency() * 2;
     // 开始计时
     const auto start = std::chrono::high_resolution_clock::now();
-    std::vector<std::future<RemoveResult>> threads;
     // 如果输入的只有一个路径
-    if (argc == 1 && fs::is_directory(argv[1])) {
-        //const auto directoryPath = fs::path(argv[1]);
-        //const std::size_t fileCount = countFilesInDirectory(directoryPath);
-        //std::cout << "The directory contains " << fileCount << " file(s)." << std::endl;
-
-        // 如果当前目录里面还是只有一个目录，则继续寻找，直到找到一个目录下面有多个目录?
-
-        // 如果是目录，为每个子路径创建线程
-        //for (const auto& entry : fs::recursive_directory_iterator(argv[1])) {
-        for (const auto& entry : fs::directory_iterator(argv[1])) {
-            threads.emplace_back(std::async(std::launch::async | std::launch::deferred, removeData, entry.path()));
-            if (threads.size() >= maxThreadCount) {
-                syncWait(threads);
-            }
-        }
+    if (argc == 2 && fs::is_directory(argv[1])) {
+        printResult(removeData(argv[1]));
     }
     else {
+        std::vector<std::future<RemoveResult>> threads;
         // 解析命令行参数并创建线程
         for (int i = 1; i < argc; ++i) {
             threads.emplace_back(std::async(std::launch::async | std::launch::deferred, removeData, fs::path(argv[i])));
+            if (threads.size() > std::thread::hardware_concurrency()) {
+                syncWait(threads);
+            }
         }
+        // 等待所有线程完成
+        syncWait(threads);
     }
-    // 等待所有线程完成
-    syncWait(threads);
     // 结束计时
     const auto end = std::chrono::high_resolution_clock::now();
     // 计算耗时
     const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-
-    // 打印耗时
-    std::cout << "Time taken: " << duration.count() << " milliseconds\n";
+    printYellow(std::format("Time taken {}ms", duration.count()));
     return 0;
 }
